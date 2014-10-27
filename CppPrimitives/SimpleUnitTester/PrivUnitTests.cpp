75040c563ebdbeacde623601cacb1e766b79aa64
Jibran Syed <jisyed14@gmail.com>
2014-10-27 09:57:52 -0500

    Tried to make unit tests more compact

--- CppPrimitives/SimpleUnitTester/PrivUnitTests.cpp
+++ CppPrimitives/SimpleUnitTester/PrivUnitTests.cpp
@@ -6,15 +6,21 @@
 #include "UnitTest.h"
 using namespace SimpleUnitTestBed;
 
+// Macro for error details
+#define ERROR_DETAILS(expected, actual) std::cout << "Expected: " \
+<< (expected) << ", but actually got: " << (actual) <<std::endl; 
+
+#define EQUAL_CHECK(expected, actual) if(actual != expected) \
+			{ \
+				ERROR_DETAILS(expected, actual); \
+				return 1; \
+			}
+
 /////////////////////////////////////////////
 // Include all classes you want to test here:
 #include "../Int.h"
 using namespace PrimitivesAsObjects;
 /////////////////////////////////////////////
-
-// Macro for error details
-#define ERROR_DETAILS(expected, actual) std::cout << "Expected: " \
-<< (expected) << ", but actually got: " << (actual) <<std::endl; 
 
 // NOTE: All unit tests return 0 on success, 1 on failure
 void SimpleUnitTester::createAllTests(UnitTestList& allUnitTests)
@@ -27,16 +33,13 @@ void SimpleUnitTester::createAllTests(UnitTestList& allUnitTests)
 		"Int", "Specialized Ctor with = sign",
 		[]()
 		{
+			// -------------------------
 			int expected = 64;
 			Int i = expected;
 
-			if(i.get() != expected)
-			{
-				// Error message details
-				ERROR_DETAILS(expected, i.get());
-				return 1;
-			}
 
+			// -------------------------
+			EQUAL_CHECK(expected, i.get());
 			return 0;
 		}
 	));
@@ -45,17 +48,13 @@ void SimpleUnitTester::createAllTests(UnitTestList& allUnitTests)
 		"Int", "Copy Ctor",
 		[]()
 		{
+			// -------------------------
 			int expected = 64;
 			Int i(expected);
 			Int j = i;
 
-			if(j.get() != expected)
-			{
-				// Error message details
-				ERROR_DETAILS(expected, j.get());
-				return 1;
-			}
-
+			// -------------------------
+			EQUAL_CHECK(expected, j.get());
 			return 0;
 		}
 	));
@@ -64,18 +63,14 @@ void SimpleUnitTester::createAllTests(UnitTestList& allUnitTests)
 		"Int", "Assignment Operator",
 		[]()
 		{
+			// -------------------------
 			int expected = 64;
 			Int i(expected);
 			Int j;
 			j = i;
 
-			if(j.get() != expected)
-			{
-				// Error message details
-				ERROR_DETAILS(expected, j.get());
-				return 1;
-			}
-
+			// -------------------------
+			EQUAL_CHECK(expected, j.get());
 			return 0;
 		}
 	));
@@ -84,17 +79,13 @@ void SimpleUnitTester::createAllTests(UnitTestList& allUnitTests)
 		"Int", "Cast to primitive int",
 		[]()
 		{
+			// -------------------------
 			int expected = 64;
 			Int i(expected);
 			int j = i;
 
-			if(j != expected)
-			{
-				// Error message details
-				ERROR_DETAILS(expected, j);
-				return 1;
-			}
-
+			// -------------------------
+			EQUAL_CHECK(expected, j);
 			return 0;
 		}
 	));
